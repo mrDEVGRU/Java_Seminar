b6415d55be302bb263f165c5588f73fa28250501
mrDEVGRU <mrdevgru@gmail.com>
2023-07-06 01:24:17 +0400

    Seminar_Java

---
+++ Seminar_java_1/Program1.java
@@ -0,0 +1,7 @@
+package Seminar_java_1;
+
+public class Program1 {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ Seminar_java_1/Program4.java
@@ -0,0 +1,7 @@
+package Seminar_java_1;
+
+public class Program4 {
+    public static void main(String[] args) {
+
+    }
+}
