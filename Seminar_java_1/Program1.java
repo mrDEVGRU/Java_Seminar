103b72113a4c4ef1be49f0f038093d404460373b
mrDEVGRU <mrdevgru@gmail.com>
2023-07-06 02:36:39 +0400

    Program1, Program2

--- Seminar_java_1/Program1.java
+++ Seminar_java_1/Program1.java
@@ -1,7 +1,20 @@
 package Seminar_java_1;
 
-public class Program1 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Program1 {
+    // Вычислить n-ое треугольного число (сумма чисел от 1 до n), n! (произведение
+    // чисел от 1 до n)
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input number: ");
+        int number = sc.nextInt();
+        System.out.printf("T number: %s\n", getNumber(number));
+
+        sc.close();
+    }
+
+    static int getNumber(int n) {
+        return n * (n + 1) / 2;
     }
 }
