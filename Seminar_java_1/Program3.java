f7c23db5cbdbda4fd1a4625219aeb5e71d06166e
mrDEVGRU <mrdevgru@gmail.com>
2023-07-14 14:39:08 +0400

    Program3

--- Seminar_java_1/Program3.java
+++ Seminar_java_1/Program3.java
@@ -1,7 +1,39 @@
 package Seminar_java_1;
 
-public class Program3 {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Program3 {
+    // 3) Реализовать простой калькулятор
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input ops: ");
+        String s = sc.nextLine();
+        System.out.print("Input number: ");
+        int number1 = sc.nextInt();
+        System.out.print("Input number: ");
+        int number2 = sc.nextInt();
+
+        System.out.print(getResult(number1, number2, s));
+
+        sc.close();
+    }
+
+    public static Integer getResult(int number1, int number2, String s) {
+        int result = 0;
+        switch (s) {
+            case "+":
+                result = number1 + number2;
+                break;
+            case "-":
+                result = number1 - number2;
+                break;
+            case "*":
+                result = number1 * number2;
+                break;
+            case "/":
+                result = number1 / number2;
+                break;
+        }
+        return result;
     }
 }
--- Seminar_java_1/Program4.java
+++
@@ -1,7 +0,0 @@
-package Seminar_java_1;
-
-public class Program4 {
-    public static void main(String[] args) {
-
-    }
-}
